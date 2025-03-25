d942d39459fec2d50107778df86da4ceefbf2e0c
ankkarp <anna.a.k.2002@gmail.com>
2025-03-25 23:44:29 +0300

    melbanks done

--- melbanks.py
+++ melbanks.py
@@ -5,6 +5,7 @@ import torch
 from torch import nn
 import torchaudio
 from torchaudio import functional as F
+import matplotlib.pyplot as plt
 
 
 class LogMelFilterBanks(nn.Module):
@@ -82,8 +83,8 @@ class LogMelFilterBanks(nn.Module):
                 where n_frames is a function of the window_length, hop_length and length of audio
         """
         spectogram = self.spectrogram(x)
-        if spectogram.is_complex():
-            spectogram = torch.abs(spectogram) ** 2
+        if self.return_complex:
+            spectogram = torch.abs(spectogram) ** self.power
         else:
             spectogram = (spectogram[..., 0] ** 2 + spectogram[..., 1] ** 2) ** (self.power / 2)
         filterbanks = torch.matmul(self.mel_fbanks.T, spectogram)
@@ -94,7 +95,7 @@ class LogMelFilterBanks(nn.Module):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input-file', type=str, help='Path to input WAV-file', default='posnanie.wav')
+    parser.add_argument('-i', '--input-file', type=str, help='Path to input WAV-file')
     args = parser.parse_args()
 
     signal, sr = torchaudio.load(args.input_file)
@@ -104,6 +105,12 @@ if __name__ == "__main__":
         n_mels=80
     )(signal)
     logmelbanks = LogMelFilterBanks()(signal)
+    fig, axes = plt.subplots(2, figsize=(10, 7))
+    axes[0].imshow(logmelbanks[0], aspect='auto')
+    axes[0].set_title('HW')
+    axes[1].imshow(torch.log(melspec + 1e-6).numpy()[0], aspect='auto')
+    axes[1].set_title('Torch (reference)')
+    plt.savefig('plots.png')
 
     assert torch.log(melspec + 1e-6).shape == logmelbanks.shape
     assert torch.allclose(torch.log(melspec + 1e-6), logmelbanks)
